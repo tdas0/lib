e2357a1a8733e877c9854e5778c3c03e99ca4f30
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-02-16 21:26:01 -0300

    header

---
+++ contest/header.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpl;
+typedef vector<int> vi;

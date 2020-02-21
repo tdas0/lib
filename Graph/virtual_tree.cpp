888dd31defdb285ec4229cff9c2c132c7dbba8da
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-02-21 08:40:36 -0300

    virtual_tree

---
+++ Graph/virtual_tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Author: Tiago Domingos
+ * Date: 2020-02-21
+ * Description: Cria uma arvore isometrica com |K|log(|K|) nós
+ * Time: O(Klog(K))
+ * Usage: mandar o conjunto de nós que a arvore tem que ter, e implementar uma função
+ * lca(x,y) e dist(x,y)
+ * Status:tested
+ */
+
+
+
+struct edge{
+	int x , y , z;
+}
+
+pair<vector<edge> ,int> build_virtual_tree(vi K){
+	auto f = [&](int a , int b){
+		return in[a] < in[b];
+	};
+	sort(all(K) , f);
+	int m = sz(K);
+	rep(i,1,m){
+		K.push_back(lca(K[i] , K[i+1]));
+	}
+	sort(all(K) , f);
+	K.erase(unique(all(K)) , K.end());
+	rep(i,0,sz(K) -1){
+		int z = lca(K[i] , K[i+1]);
+		e.push_back({K[i+1] , z ,dist(z,K[i+1])});
+	}
+	return {e ,K[0]};
+}

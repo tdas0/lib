b5c10f8794602b0a2429224c360b06778d93bfe5
MatheusLealv <matheusleal9696@gmail.com>
2020-03-23 02:26:42 -0300

    Update virtual_tree.cpp

--- Graph/virtual_tree.cpp
+++ Graph/virtual_tree.cpp
@@ -5,6 +5,8 @@
  * Time: O(Klog(K))
  * Usage: mandar o conjunto de nós que a arvore tem que ter, e implementar uma função
  * lca(x,y) e dist(x,y)
+ * K = vertices para adicionar na virtual tree
+ * edge = (x,y,z). Aresta de x para y com custo z
  * Status:tested
  */
 
@@ -13,19 +15,20 @@
 #include "graph.cpp"
 
 pair<vector<edge> ,int> build_virtual_tree(vi K){
-	auto f = [&](int a , int b){
-		return in[a] < in[b];
-	};
-	sort(all(K) , f);
-	int m = sz(K);
-	rep(i,1,m){
-		K.push_back(lca(K[i] , K[i+1]));
-	}
-	sort(all(K) , f);
-	K.erase(unique(all(K)) , K.end());
-	rep(i,0,sz(K) -1){
-		int z = lca(K[i] , K[i+1]);
-		e.push_back({K[i+1] , z ,dist(z,K[i+1])});
-	}
-	return {e ,K[0]};
+    auto f = [&](int a , int b){
+        return in[a] < in[b];
+    };
+    vector<edge> e;
+    sort(all(K) , f);
+    int m = sz(K);
+    rep(i,1,m){
+        K.push_back(lca(K[i] , K[i-1]));
+    }
+    sort(all(K) , f);
+    K.erase(unique(all(K)) , K.end());
+    rep(i,0,sz(K) -1){
+        int z = lca(K[i] , K[i+1]);
+        e.push_back({K[i+1] , z ,dist(z,K[i+1])});
+    }
+    return {e ,K[0]};
 }

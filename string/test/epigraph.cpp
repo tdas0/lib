b2d9c167b86e8f3e392fa716388c4e0225b90da4
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-03-29 00:16:42 -0300

    automaton atualizado

--- string/test/epigraph.cpp
+++ string/test/epigraph.cpp
@@ -1,5 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpl;
+typedef vector<int> vi;
+ll modpow(ll b, ll e ,ll mod) {
+	ll ans = 1;
+	for (; e; b = b * b % mod, e /= 2)
+		if (e & 1) ans = ans * b % mod;
+	return ans;
+}
+
 struct suffix_automaton{
-	enum{alpha  = 26 , first = 'a'};
+	enum{alpha  = 256 , first = 0};
 	struct node{
 		int len, link;
 		vi next;
@@ -46,12 +71,12 @@ struct suffix_automaton{
 		}
 		return t;
 	}
-	bool find(const string &s){
-		int p = 0;
-		rep(j,0,sz(s)){
-			if(!st[p].next[s[j]-first]) return false;
-			p = st[p].next[s[j]-first];
-		}
-		return true;
-	}
 };
+
+int32_t main(){
+	suffix_automaton sa;
+	string s;
+	cin >> s;
+	rep(i,0,sz(s)) sa.extend(s[i]);
+	cout << sa.substrings << endl;
+}	
--- string/test/nenokku.cpp
+++ string/test/nenokku.cpp
@@ -1,3 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpl;
+typedef vector<int> vi;
+ll modpow(ll b, ll e ,ll mod) {
+	ll ans = 1;
+	for (; e; b = b * b % mod, e /= 2)
+		if (e & 1) ans = ans * b % mod;
+	return ans;
+}
 struct suffix_automaton{
 	enum{alpha  = 26 , first = 'a'};
 	struct node{
@@ -55,3 +79,21 @@ struct suffix_automaton{
 		return true;
 	}
 };
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0) , cout.tie(0);
+	string a , b;
+	suffix_automaton sa;
+	while(cin >> a >> b){
+		rep(i,0,sz(b)){
+			if(b[i] >= 'A' && b[i] <='Z') b[i] = b[i] - 'A' + 'a';
+		}
+		if(a == "?"){
+			cout << (sa.isin(b) ? "YES" : "NO") << "\n";
+		}
+		else{
+			rep(i,0,sz(b)) sa.extend(b[i]);
+		}
+	}
+}

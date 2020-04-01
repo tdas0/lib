23dcd071de55bdce22ecb25d96a2215b6e024502
MatheusLealv <matheusleal9696@gmail.com>
2020-04-01 18:31:08 -0300

    Create kth_BIT.cpp

---
+++ data_structure/kth_BIT.cpp
@@ -0,0 +1,24 @@
+/**
+ * Author: Leal
+ * Date: 2009-10-30
+ * License: ??
+ * Source: bit kth
+ * Description: Pega o kth menor elemento que esta no BIT.
+ * Time: Both operations are $O(\log N)$.
+ */
+int bit[N];
+ 
+void upd(int pos, int val){
+    for(int i = pos; i < N; i += (i&-i)) bit[i] += val;
+}
+ 
+int kth(int x){
+   int resp = 0;
+    x--;
+    for(int i = 20; i >= 0; i--){
+        if(resp + (1<<i) >= N) continue;
+        if(bit[resp + (1 << i)] <= x) 
+            x -= bit[resp + (1 << i)], resp = resp + (1 << i);
+    }
+    return resp + 1;
+}

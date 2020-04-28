ab3c281ab9b7ccbb934cd4520f02d2469372ffb6
Tiago Domingos <tiago.dsouza0@gmail.com>
2020-04-28 20:01:38 -0300

    SCC

---
+++ Graph/SCC.cpp
@@ -0,0 +1,34 @@
+/**
+ * Author: Tiago
+ * Date: 2020-04-28
+ * Description: Computa a SCC, comp[x] diz a componente que o nó x está, comps são as componentes
+ * O(n)
+ * Status: tested
+ */
+
+struct SCC{
+	vector< vi> g , comps;
+	vi val, z, cont, comp;
+	int Time =0, ncomps= 0 , n; 
+	SCC(vector<vi> g, int n ):n(n) , g(g) , val(n+1,0), comp(n+1, -1) {}
+	int dfs(int j){
+		int low = val[j] = ++Time , x ; z.push_back(j);
+		trav(e, g[j]) if(comp[e] < 0)
+			low = min(low , val[e] ?: dfs(e));
+		if(low == val[j]){
+			do{
+				x = z.back() ; z.pop_back();
+				comp[x] = ncomps;
+				cont.push_back(x);
+			} while(x != j);
+			comps.push_back(cont) , cont.clear();
+			ncomps++;
+		}
+		return val[j] = low;
+	}
+	void scc(){
+		val.assign(n+1,0); comp.assign(n+1,-1);
+		Time = ncomps = 0 ;
+		rep(i,1,n+1) if(comp[i] < 0 ) dfs(i);
+	}
+};
---
+++ Graph/test/noble.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpl;
+typedef vector<int> vi;
+ll modpow(ll b, ll e ,ll mod) {
+	ll ans = 1;
+	for (; e; b = b * b % mod, e /= 2)
+		if (e & 1) ans = ans * b % mod;
+	return ans;
+}
+
+

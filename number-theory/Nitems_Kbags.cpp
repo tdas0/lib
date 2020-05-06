d764eb53e148b1eb56c0df4947a392041e4e7bf2
NaimSS <55881611+NaimSS@users.noreply.github.com>
2020-05-06 14:12:18 -0300

    Update Nitems_Kbags.cpp

--- number-theory/Nitems_Kbags.cpp
+++ number-theory/Nitems_Kbags.cpp
@@ -12,16 +12,16 @@
 //considerando que cada mochila tem pelo menos 1 objeto
 //choose[i] = choose(k, i) = k escolhe i
 // ans = somatorio ( (-1)^i * (k choose i) * (k-1)^n)
-int get(int n, int k){
-	int ans = 0;
-	vector<int> choose(k+1);
+ll get(int n, int k){
+	ll ans = 0;
+	vector<ll> choose(k+1);
 	choose[0] = 1;
 	for(int j = 1; j <= k; j++){
 		choose[j] = (choose[j-1] * (k-j+1)) % mod;
 		choose[j] = (choose[j]*poww(j, mod-2))%mod;
 	}
 	for(int i = 0; i<= k; i++){
-		int S = (i%2 == 0 ?1:-1);
+		ll S = (i%2 == 0 ?1:-1);
 		S = S*choose[i]*poww(k-i, n);
 		S %= mod;
 		if(S<0)S += mod;

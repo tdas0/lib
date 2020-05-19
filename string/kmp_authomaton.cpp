71d98b79ab12825ba6f0e55fe66343b92e0b5551
NaimSS <55881611+NaimSS@users.noreply.github.com>
2020-05-19 12:32:09 -0300

    Create kmp_authomaton.cpp

---
+++ string/kmp_authomaton.cpp
@@ -0,0 +1,36 @@
+/* Source: https://cp-algorithms.com/string/prefix-function.html
+ * Description: Builds automaton on string in O(alpha * |S|)
+ * aut[i][c] -> next position if on position 'i' and adding the c-th character 
+ */
+ 
+ // build prefix function
+ vector<int> prefix_function(string s) {
+    int n = (int)s.length();
+    vector<int> pi(n);
+    rep(i,1,n) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+vi pi;
+vector<vi> aut;
+void compute_automaton(string s,int alpha = 26) {
+    s += '#'; 
+    int n = s.size();
+     pi = prefix_function(s);
+    aut.assign(n+1, vector<int>(alpha));
+    rep(i,0,n){
+        rep(c,0,alpha){
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[pi[i-1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
+        }
+    }
+}

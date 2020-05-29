b1d358c2174cbff68aa4c2f65266d9949c3c4b2b
Tiago Domingos <35474192+tdas0@users.noreply.github.com>
2020-05-29 16:25:47 -0300

    Update d&c.cpp

--- dp/d&c.cpp
+++ dp/d&c.cpp
@@ -4,40 +4,31 @@
  * !! compute(1,n,1,n) vs compute(i,n,i,n)  
  * Tested on https://codeforces.com/contest/321/submission/81919245
 */
-
-vector<long long> dp_before(N), dp_cur(N);
-const int inf = ;
- 
-long long cost(int l,int r); 
- 
-// compute dp_cur[l], ... dp_cur[r] (inclusive) 
-void compute(int l, int r, int optl, int optr)
-{
-    if (l > r)
-        return;
-    int mid = (l + r) >> 1;
-    pair<int, int> best = {inf, -1}; // se for maximizar muda pra -inf
-  
-    rep(k,optl,min(mid,optr)+1){
-        best = min(best, { dp_before[k-1] + cost(k,mid), k});
-    }
- 
-    dp_cur[mid] = best.first;
-    int opt = best.second;
- 
-    compute(l, mid - 1, optl, opt);
-    compute(mid + 1, r, opt, optr);
-}
-
-void init(){
-   rep(i,1,n+1){
-    dp_before[i] = cost(1,i);
-  }
- 
-  rep(i,2,k+1){
-    rep(j,i,n+1)dp_cur[j] = inf;
-    compute(i,n,i,n);
-    swap(dp_cur,dp_before);  
-  }
-  
-}
+struct DP{
+	int n , inf; 
+	vi dp[2];
+	DP(int n , int inf) : n(n) , inf(inf) {dp[0] = vi(n+1) , dp[1] = vi(n+1);}
+	int cost(int i,int j);
+	// compute dp_cur[l], ... dp_cur[r] (inclusive) 
+	void compute(int l, int r, int optl, int optr){
+	    if (l > r) return;
+	    int mid = l + (r-l)/2;
+	    pair<int, int> best = {inf, -1}; // se for maximizar muda pra -inf
+	    rep(k,optl,min(mid,optr)+1){
+	        best = min(best, { dp[0][k-1] + cost(k,mid), k});
+	    }
+	    dp[1][mid] = best.first; int opt = best.second;
+	    compute(l, mid - 1, optl, opt);
+	    compute(mid + 1, r, opt, optr);
+	}
+	void solve(int k){
+		rep(i,1,n+1){
+			dp[0][i] = cost(1,i);
+		}
+		rep(i,2,k+1){
+			rep(j,i,n+1) dp[1][j] = inf;
+			compute(i,n,i,n) ;
+			swap(dp[0] , dp[1]);
+		}
+	}
+};

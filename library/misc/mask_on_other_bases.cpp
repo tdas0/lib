edfe5274429aea55ae2db587b2b59d37cf133863
NaimSS <55881611+NaimSS@users.noreply.github.com>
2020-07-31 22:30:46 -0300

    Create mask_on_other_bases.cpp

---
+++ library/misc/mask_on_other_bases.cpp
@@ -0,0 +1,11 @@
+/* Descricao: Permite fazer "bitmask" para bases > 2
+ * Get Mask pega o valor na i-esima posicao de mask na base K
+ * Tested on http://opentrains.snarknews.info/~ejudge/sn_sh.cgi?data=result_team&sid=5971f41c589840b9
+ */
+ 
+ const int k = ;//base
+ int pwrk[logk];
+
+ int get_mask(int mask, int i){
+	return (mask/powk[i]) % k;
+}

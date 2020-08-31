cf9001d25bf3361c80018c44d96aaeca452717a4
NaimSS <55881611+NaimSS@users.noreply.github.com>
2020-08-31 20:15:32 -0300

    Create f.cpp

---
+++ contests/seerc/19/f.cpp
@@ -0,0 +1,67 @@
+// upsolving
+#include <bits/stdc++.h>
+#define ld long double
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb(x) push_back(x)
+#define mp(a,b) make_pair(a,b)
+#define ms(v,x) memset(v,x,sizeof(v))
+#define all(v) v.begin(),v.end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define td(v) v.begin(),v.end()
+#define sz(v) (int)v.size()
+#define int long long
+using namespace std;
+typedef vector<int> vi;
+#define y1 abacaba
+#define left sefude
+#define db(x) cerr << #x <<" == "<<x << endl;
+#define db2(x,y) cerr<<#x <<" == "<<x<<", "<<#y<<" == "<<y<<endl;
+#define db3(x,y,z) cerr << #x<<" == "<<x<<", "<<#y<<" == "<<y<<", "<<#z<<" == "<<z<<endl; 
+typedef long long ll;
+typedef pair<int,int> pii;
+
+inline ll mod(ll n, ll m ){ ll ret = n%m; if(ret < 0) ret += m; return ret; }
+ll gcd(ll a, ll b){return (b == 0LL ? a : gcd(b, a%b));}
+ll exp(ll a,ll b,ll m){
+    if(b==0LL) return 1LL;
+    if(b==1LL) return mod(a,m);
+    ll k = mod(exp(a,b/2,m),m);
+    if(b&1LL){
+        return mod(a*mod(k*k,m),m);
+    }
+    else return mod(k*k,m);
+}
+
+
+const int N = 100100;
+vi g[N];
+
+int dfs(int v,int p = -1){//number of unmatched nodes
+  int cur = 0;
+  for(int to : g[v])if(to!=p){
+    cur+=dfs(to,v);
+  }
+  if(cur == 0)return 1;
+  return cur - 1;
+}
+
+int32_t main(){
+  fastio;
+  int n;
+  cin>>n;
+  rep(i,0,n-1){
+    int a,b;
+    cin >> a>> b;
+    g[a].pb(b);
+    b[g].pb(a);
+  }
+  cout << (dfs(1) ? "Alice" : "Bob") << endl;
+  // math -> gcd it all
+  // Did u check N=1? Did you switch N,M?
+}

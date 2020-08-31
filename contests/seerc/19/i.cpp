6d8ad8a939913706d2c7591ab1d1c5ea223e5a6c
NaimSS <55881611+NaimSS@users.noreply.github.com>
2020-08-31 20:13:24 -0300

    Create i.cpp

---
+++ contests/seerc/19/i.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <climits>
+
+using namespace std;
+
+#define F first
+#define S second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define all(x) begin(x) , end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpl;
+typedef vector<int> vi;
+
+ll modpow(ll b, ll e ,ll mod){
+	ll ans = 1;
+	for (; e; b = b * b % mod, e /= 2)
+		if (e & 1) ans = ans * b % mod;
+	return ans;
+}
+const int N = 1005;
+int n;
+int a[N] , b[N];
+
+int32_t main(){
+	scanf("%d" , &n);
+	for(int i = 1 ;i <= n; i ++)
+		scanf("%d" , &a[i]);
+	for(int i =1 ;i <= n; i++)
+		scanf("%d" , &b[i]);
+	ll ans = 0;
+	for(int i = 1 ;i <= n; i ++){
+		ll curr = (ll) 1e18;
+		for(int j = 1;  j <= n; j ++)
+			curr = min(curr, (ll)abs(a[i] - b[j]));
+		ans = max(ans, curr);
+	}
+	printf("%lld\n" , ans);
+}
+
+/* clever stuff:
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and STAY ORGANIZED
+	* Keep it simple stupid
+	* WRITE STUFF DOWN
+	* math -> gcd / lcm / divisors? 
+*/

fe002887054d1db890c9e2af05db9db7c020bad1
NaimSS <55881611+NaimSS@users.noreply.github.com>
2020-10-07 12:01:04 -0300

    Create rmq.cpp

---
+++ library/data_structure/rmq.cpp
@@ -0,0 +1,30 @@
+/**
+ * Description: 1D range minimum query. Can also do queries 
+ 	* for any associative operation in $O(1)$ with D\&C
+ * Source: KACTL
+ * Verification: 
+	* https://cses.fi/problemset/stats/1647/
+	* http://wcipeg.com/problem/ioi1223
+	* https://pastebin.com/ChpniVZL
+ * Memory: O(N\log N)
+ * Time: O(1)
+ */
+ 
+template<class T> struct RMQ { // floor(log_2(x))
+  int level(int x) { return 31-__builtin_clz(x); } 
+  vector<T> v; vector<vi> jmp;
+  int comb(int a, int b) { // index of min
+    return v[a]==v[b]?min(a,b):(v[a]<v[b]?a:b); } 
+  void init(const vector<T>& _v) { // 0-index
+    v = _v; jmp = {vi(sz(v))}; iota(all(jmp[0]),0);
+    for (int j = 1; 1<<j <= sz(v); ++j) {
+      jmp.pb(vi(sz(v)-(1<<j)+1));
+      rep(i,0,sz(jmp[j])) jmp[j][i] = comb(jmp[j-1][i],
+                  jmp[j-1][i+(1<<(j-1))]);
+    }
+  }
+  int index(int l, int r) { // get index of min element
+    assert(l <= r); int d = level(r-l+1);
+    return comb(jmp[d][l],jmp[d][r-(1<<d)+1]); }
+  T query(int l, int r) { return v[index(l,r)]; } // [l,r], 0-index
+};

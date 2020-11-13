301d563ab1ee18d78fc7ef22f532061b362e0836
Tiago Domingos <35474192+tdas0@users.noreply.github.com>
2020-11-13 17:40:32 -0300

    Update 2cc.cpp

--- library/Graph/2cc.cpp
+++ library/Graph/2cc.cpp
@@ -1,3 +1,24 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2017-04-17
+ * License: CC0
+ * Source: folklore
+ * Description: Finds all biconnected components in an undirected graph, and
+ *  runs a callback for the edges in each. In a biconnected component there
+ *  are at least two distinct paths between any two nodes. Note that a node can
+ *  be in several components. An edge which is not in a component is a bridge,
+ *  i.e., not part of any cycle.
+ * Usage:
+ *  int eid = 0; ed.resize(N);
+ *  for each edge (a,b) {
+ *    ed[a].emplace_back(b, eid);
+ *    ed[b].emplace_back(a, eid++); }
+ *  bicomps([\&](const vi\& edgelist) {...});
+ * Time: O(E + V)
+ * Status: tested during MIPT ICPC Workshop 2017
+ */
+#pragma once
+
 vi num, st;
 vector<vector<pii>> ed;
 int Time;

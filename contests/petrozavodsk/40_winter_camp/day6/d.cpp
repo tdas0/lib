f0922afa208008969510afe73222a6b90d30dc78
NaimSS <55881611+NaimSS@users.noreply.github.com>
2021-02-04 08:58:54 -0300

    Create d.cpp

---
+++ contests/petrozavodsk/40_winter_camp/day6/d.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef pair<ll,ll> pll;
+typedef vector<string> vs;
+typedef vector<pll> vpl;
+typedef vector<int> vi;
+std::mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
+
+
+typedef unsigned long long ull;
+ull modmul(ull a, ull b, ull M) {
+	ll ret = a * b - M * ull(1.L / M * a * b);
+	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = modmul(b, b, mod), e /= 2)
+		if (e & 1) ans = modmul(ans, b, mod);
+	return ans;
+}
+
+bool isPrime(ull n) {
+	if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+	ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+	    s = __builtin_ctzll(n-1), d = n >> s;
+	for (ull a : A) {   // ^ count trailing zeroes
+		ull p = modpow(a%n, d, n), i = s;
+		while (p != 1 && p != n - 1 && a % n && i--)
+			p = modmul(p, p, n);
+		if (p != n-1 && i != s) return 0;
+	}
+	return 1;
+}
+
+int32_t main(){
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for(int i = 0 ; i < n; i ++){
+		cin >> v[i];
+	}
+	if(n < 15){
+		bool ok = false;
+		for(int i = 1 ; i < (1<<n) ; i ++){
+			ll sum = 0;
+				for(int j =0  ; j< n; j ++){
+					if((1<<j) & i)
+						sum += v[j];
+				}
+			if(sum != 1)
+				ok |= (!isPrime(sum));
+			if(ok)
+				break;
+		}
+		cout << (ok ? "Yes" : "No") << endl;
+	}
+	else{
+		cout << "Yes" << endl;
+	}
+}

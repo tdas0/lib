dbb707301b532ffc6ec64e9e5913e6757b30bb12
NaimSS <55881611+NaimSS@users.noreply.github.com>
2021-02-09 15:05:15 -0300

    Create i.cpp

---
+++ contests/petrozavodsk/40_winter_camp/day9/i.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define ld long double
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define mp(a,b) make_pair(a,b)
+#define ms(v,x) memset(v,x,sizeof(v))
+#define all(v) v.begin(),v.end()
+#define ff first
+#define ss second
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define per(i, a, b) for(int i = b-1; i>=a ; i--)
+#define trav(a, x) for(auto& a : x)
+#define allin(a , x) for(auto a : x)
+#define Unique(v) sort(all(v));v.erase(unique(all(v)),v.end());
+#define sz(v) ((int)v.size())
+//#define int long long  
+using namespace std;
+typedef vector<int> vi;
+#define y1 abacaba
+//#define left oooooopss
+#define db(x) cerr << #x <<" == "<<x << endl;
+#define db2(x,y) cerr<<#x <<" == "<<x<<", "<<#y<<" == "<<y<<endl;
+#define db3(x,y,z) cerr << #x<<" == "<<x<<", "<<#y<<" == "<<y<<", "<<#z<<" == "<<z<<endl;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+std::mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
+inline ll mod(ll n, ll m ){ ll ret = n%m; if(ret < 0) ret += m; return ret; }
+ll gcd(ll a, ll b){return (b == 0LL ? a : gcd(b, a%b));}
+ll exp(ll b,ll e,ll m){
+  b%=m;
+  ll ans = 1;
+  for (; e; b = b * b % m, e /= 2)
+      if (e & 1) ans = ans * b % m;
+  return ans;
+}
+ 
+int f(int x){
+  return (x==1 ? 0 : (1 + f((x+1)/2)));
+}
+int32_t main(){
+  fastio;
+  int n;
+  cin >> n;
+  int dir = 0,cnt=0;
+  vi a(n+1);
+  rep(i,1,n+1)cin >> a[i];
+
+  for(int i=1;i<=n;){
+    int x = a[i];
+    while(i<=n){
+      if(dir==0 and a[i] < x)break;
+      if(dir==1 and a[i] > x)break;
+      x = a[i++];
+    }
+    dir^=1;cnt++;
+  }
+  
+  cout << f(cnt) << endl;
+  
+  // math -> gcd it all
+  // Did u check N=1? Did you switch N,M?
+}

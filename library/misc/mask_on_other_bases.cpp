7d718004c4d2b735321e644f216464ee828ac79f
NaimSS <55881611+NaimSS@users.noreply.github.com>
2021-02-23 18:56:22 -0300

    Update mask_on_other_bases.cpp

--- library/misc/mask_on_other_bases.cpp
+++ library/misc/mask_on_other_bases.cpp
@@ -9,3 +9,12 @@
  int get_mask(int mask, int i){
 	return (mask/powk[i]) % k;
 }
+
+// exemplo: SOS - cnt(v) = soma de Ai onde todo bit não nulo de i é igual ao de v (v contem i)
+
+rep(j,0,m)rep(mask,1,p3[m]){ // p3 -> potencias de 3 (problema C da CCPC 2016/2017 finals
+    int here = (mask/p3[j])%3;
+    if(here == 0)continue;
+    int nv = mask - here * p3[j];
+    cnt[nv]+=cnt[mask];
+  }

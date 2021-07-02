1c76f5f7a87e3221797d1551fb10ed1dfe82dfdb
NaimSS <55881611+NaimSS@users.noreply.github.com>
2021-07-02 12:17:14 -0300

    Update minkowski.h

--- library/geometry/minkowski.h
+++ library/geometry/minkowski.h
@@ -36,3 +36,37 @@ vector<P> minkowski(const vector<P> &A ,const vector<P> &B){
 	return e;
 }
 // warning : e contains colinear points, you can easily remove in the merge step 
+
+
+// cp algo:
+void reorder_polygon(vector<pt> & P){
+    size_t pos = 0;
+    for(size_t i = 1; i < P.size(); i++){
+        if(P[i].y < P[pos].y || (P[i].y == P[pos].y && P[i].x < P[pos].x))
+            pos = i;
+    }
+    rotate(P.begin(), P.begin() + pos, P.end());
+}
+
+vector<pt> minkowski(vector<pt> P, vector<pt> Q){
+    // the first vertex must be the lowest
+    reorder_polygon(P);
+    reorder_polygon(Q);
+    // we must ensure cyclic indexing
+    P.push_back(P[0]);
+    P.push_back(P[1]);
+    Q.push_back(Q[0]);
+    Q.push_back(Q[1]);
+    // main part
+    vector<pt> result;
+    size_t i = 0, j = 0;
+    while(i < P.size() - 2 || j < Q.size() - 2){
+        result.push_back(P[i] + Q[j]);
+        auto cross = (P[i + 1] - P[i]).cross(Q[j + 1] - Q[j]);
+        if(cross >= 0)
+            ++i;
+        if(cross <= 0)
+            ++j;
+    }
+    return result;
+}

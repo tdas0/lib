a484940551f02b9e7d38cb35ded069fd7b2c1bbb
Matheus Leal V <matheusleal9696@gmail.com>
2021-07-07 00:33:11 -0300

    Update bola_cabelo.cpp

--- library/Graph/bola_cabelo.cpp
+++ library/Graph/bola_cabelo.cpp
@@ -1,16 +1,16 @@
 /*
   tripinha[x] = 1 se é tripa (nao ciclo) 0 caso contrário
   len_ciclo[x] = tamanho do ciclo que o x liga
-  dist_ciclo[x] = distancia de x para o ciclo
+  prefixo[x] = distancia de x para o ciclo(caso x for tripa), prefixo do ciclo caso x ta no ciclo
   id_pai[x] = qual vértice partindo de x ta no ciclo
 */
 //1-indexado
-const int LOG = 20;
 
-int vis[N];
-int prox[N], grau[N], id_pai[N], dist_pai[N], dist_ciclo[N], len_ciclo[N];
-int tripinha[N]; // um vértice é tripa se nao ta no ciclo xD
+int vis[N], tin[N], tout[N], total[N], tmp_cnt=0;
+int componente[N], prox[N], grau[N], id_pai[N], dist_pai[N], dist_ciclo[N], len_ciclo[N];
+int prefixo[N], tripinha[N]; // um vértice é tripa se nao ta no ciclo xD
 vector<int> chega[N];
+vector< vector<int> > tripas;
 
 // 1-indexado
 struct bola_cabelo{
@@ -25,10 +25,14 @@ struct bola_cabelo{
 		grau[b] ++;
 	}
 	void dfs(int x, int p){
+		tin[x] = ++tmp_cnt;
 		id_pai[x] = id_pai[p];
-		dist_ciclo[x] = dist_ciclo[p] + dist_pai[x];
-		len_ciclo[x] = len_ciclo[p];
-		for(auto v: chega[x]) dfs(v, x);
+		len_ciclo[x] = len_ciclo[p]; componente[x]=componente[p];
+		for(auto v: chega[x]){
+			prefixo[v] = prefixo[x]+dist_pai[v];
+			dfs(v, x);
+		}
+		tout[x] = tmp_cnt;
 	}
 	void solve(){
 		vector<int> lista;
@@ -44,23 +48,51 @@ struct bola_cabelo{
 			id_pai[x]=x;
 			for(auto v: chega[x]) if(tripinha[v]) id_pai[v] = x;
 			if(vis[x]) continue;
-			int y = prox[x], len = 1; vis[x]=1;
+			int y = prox[x], len = 1, ant = x; vis[x]=1;
+			int sum = dist_pai[x];
+			prefixo[x] = 0;
 			vector<int> caras={x};
 			while(y != x){
+				sum += dist_pai[y];
 				vis[y]=1;
 				caras.pb(y);
 				len ++;
 				y=prox[y];
 			}
-			for(auto w: caras) len_ciclo[w] = len;
+			reverse(all(caras));
+			for(int i = 0; i < sz(caras); i++){
+				if(!i) prefixo[caras[i]] = 0;
+				else prefixo[caras[i]] = prefixo[caras[i-1]] + dist_pai[caras[i-1]];
+				len_ciclo[caras[i]] = len;
+				componente[caras[i]]=x;
+				total[caras[i]] = sum;
+			}
 		}
 		for(int x = 1; x <= numVertices; x++){
 			if(!tripinha[x] or tripinha[prox[x]]) continue;
+			prefixo[x] = dist_pai[x];
 			dfs(x,prox[x]);
 		}
 	}
   // Maior caminho começando em X sem repetir vertice
 	int longest_path(int x){
-		return len_ciclo[x] + dist_ciclo[x];
+		return len_ciclo[x] + (tripinha[x]?prefixo[x]:0);
+	}
+	int menor_dist(int from, int to){
+		if(componente[from] != componente[to]) return -1;
+		if(id_pai[from] == id_pai[to] and tripinha[to] and tripinha[from] and tin[to] <= tout[from] and tout[from] <= tout[to]){
+			if(prefixo[from] >= prefixo[to]) return prefixo[from] - prefixo[to];
+			return -1;
+		}
+		if(tripinha[from] and !tripinha[to]){
+			int cost = (prefixo[id_pai[from]] - prefixo[to]);
+			if(cost < 0) cost += total[to];
+			return prefixo[from] + cost;
+		}
+		if(!tripinha[from] and !tripinha[to]){
+			if(prefixo[from] >= prefixo[to]) return prefixo[from] - prefixo[to];
+			return total[from] - (prefixo[to] - prefixo[from]);
+		}
+		return -1;
 	}
 };

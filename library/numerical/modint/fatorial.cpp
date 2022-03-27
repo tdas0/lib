95f82dcb37d96872afd24b870763205923540f13
NaimSS <55881611+NaimSS@users.noreply.github.com>
2022-03-27 09:49:30 -0300

    Update fatorial.cpp

--- library/numerical/modint/fatorial.cpp
+++ library/numerical/modint/fatorial.cpp
@@ -1,3 +1,12 @@
+/*
+  fat[i] = (i*fat[i-1])
+  inv[i] = (inv[i+1]*(i+1)) ou inv[i] = fat[i] ^ (phi(M) - 1)
+  per_caotica[n] = (n-1)*(per_caotica[n-2] + per_caotica[n-1]) AKA Derangement
+  !n = n * (!(n-1)) + (-1)^n para n>0
+  per_caotica[n] = n! * sum(1/k! * (-1)^k)
+  sum(C(n,k)*k) = n*2^(n-1) - prova: derivar binomio (1+x)^n em x = 1
+*/
+
 Mint  fat[N];
 Mint inv[N];
 

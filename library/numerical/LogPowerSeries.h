446a3a626259cc0c387180a0a6d1384a658e4e65
Tiago Domingos <35474192+tdas0@users.noreply.github.com>
2022-06-27 22:47:07 -0300

    Create LogPowerSeries.h

---
+++ library/numerical/LogPowerSeries.h
@@ -0,0 +1,17 @@
+vl log(vl A , int n){
+	assert(sz(A) != 0 && A[0] != 0);
+	vl P(n,0) , ans(n,0);
+	vl fat(n,1);
+	for(int i = 1; i < n ; i ++){
+		fat[i] = (fat[i-1] * i)%mod;
+		P[i-1] = (A[i] * i) % mod;
+	}
+	vl F = conv(P,inverse(A,n));
+	ll ifat = modpow(fat[n-1],mod-2,mod);
+	for(int i = n-2 ; i >= 0; i --){
+		ll cur = (ifat * fat[i])%mod;
+		ifat = (ifat * (i+1))%mod;
+		ans[i+1] = (F[i] * cur)%mod;
+	}
+	return ans;
+}

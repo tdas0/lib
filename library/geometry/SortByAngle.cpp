4d89a8f34a2193b5266efe770fc4814e8e569482
NaimSS <55881611+NaimSS@users.noreply.github.com>
2022-11-01 15:04:57 -0300

    Update SortByAngle.cpp

--- library/geometry/SortByAngle.cpp
+++ library/geometry/SortByAngle.cpp
@@ -2,16 +2,17 @@
  * Author: Tiago
  * Date: 2021-02-14
  * Description:
-  Sort points by angle arround a chosen pivot, untied by distance
+ * Sort points by angle arround a chosen pivot, untied by distance.
+ * ccw, starting at quadrant x>0,y<0.
  * Time: O(n \log n)
 */
 
-	sort(all(pts) , [&](P a , P b){
-		a = a - pivot , b = b - pivot;
-		int hp1 = a < P(0,0) , hp2 = b < P(0,0);
-		if(hp1 != hp2) return hp1 < hp2;
-		if(a.cross(b) != 0){
-			return a.cross(b)  > 0;
-		}
-		return a.dist2() < b.dist2();
-	});
+sort(all(pts) , [&](P a , P b){
+	a = a - pivot , b = b - pivot;
+	int hp1 = a < P(0,0) , hp2 = b < P(0,0);
+	if(hp1 != hp2) return hp1 < hp2;
+	if(a.cross(b) != 0){
+		return a.cross(b)  > 0;
+	}
+	return a.dist2() < b.dist2();
+});

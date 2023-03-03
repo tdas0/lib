7cc5e500589fa03dd65aa6a619e8cfa763c0ca76
NaimSS <55881611+NaimSS@users.noreply.github.com>
2023-03-03 13:00:23 -0300

    Update graycode.cpp

--- library/misc/graycode.cpp
+++ library/misc/graycode.cpp
@@ -10,3 +10,31 @@ int rev_g (int g) {
 }
 
 // the bit that changed is a rod-move in hanoi-classic
+
+// generate all combinations of sets of size K of elements from 1 to N,
+// where neighbors change by only 1 element
+vector<int> ans;
+
+void gen(int n, int k, int idx, bool rev) {
+    if (k > n || k < 0)
+        return;
+
+    if (!n) {
+        for (int i = 0; i < idx; ++i) {
+            if (ans[i])
+                cout << i + 1;
+        }
+        cout << "\n";
+        return;
+    }
+
+    ans[idx] = rev;
+    gen(n - 1, k - rev, idx + 1, false);
+    ans[idx] = !rev;
+    gen(n - 1, k - !rev, idx + 1, true);
+}
+
+void all_combinations(int n, int k) {
+    ans.resize(n);
+    gen(n, k, 0, false);
+}

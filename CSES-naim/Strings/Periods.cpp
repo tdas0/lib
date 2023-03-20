7f68ac8492e94ed50d653354a92345f115a19277
Naim <queria.ser.arthur@gmail.com>
2023-03-20 15:03:49 -0300

    reorganizando

--- CSES-naim/Strings/Periods.cpp
+++
@@ -1,9 +0,0 @@
-/*
-generate the whole string by repeating the prefix.
-The last repetition may be partial
-*/
-vector<int> z = z_function(s);
-for(int j=0;j<n;j++){
-    if(z[j] == n - j)res.pb(j);
-}
-res.pb(n);

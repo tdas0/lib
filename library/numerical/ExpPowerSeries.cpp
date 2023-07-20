b52b1903d3a0fc15e3ab9e7abc6c4b22999a97bf
NaimSS <55881611+NaimSS@users.noreply.github.com>
2023-07-20 11:30:49 -0300

    Update ExpPowerSeries.cpp

--- library/numerical/ExpPowerSeries.cpp
+++ library/numerical/ExpPowerSeries.cpp
@@ -1,3 +1,22 @@
+/**
+ * Author: tdas
+ * Complexity: $O(log^2N)$ ?
+ * Description: Given a power series $A = \sum x^i \cdto A_i$, find $e^A mod x^{n+1}$ i,e. the first
+ * $n$ terms of the exponential
+ * Useful for generating functions. Example:
+ * If A(x) is a EGF, then $C(x) = A(x)^k$ is 
+ * $c_{n} = \sum_{i_{1}+i_{2}+...+i_{k}=n}\frac{n!}{i_{1}!i_{2}!...i_{k}!}a_{i_{1}}a_{i_{2}}...a_{i_{k}}$
+ * Which is basically summing all the ways to select $k$ elements. Example: Permutation with $k$ cycles. 
+ * $A(x) = \sum \frac{(n-1)!}{n!}x^n$, because there are $(n-1)!$ permutations with exaclty 1 cycle. The extra $n!$ is added to due
+ * The definition of EGF. Note that $C(x) = A(x)^k$ is the same as $C(x) = exp(k \cdot ln(A))$.
+ * Example 2: 
+ * Number of permutations where each cycle size is in $ S $. 
+ * Use $\sum_{k \ge 0}\frac{1}{k!}C(x)^{k} = [x^{n}]\exp(C(x))$, which basically sums over all possible number of cycles $k$.
+ * Here, $c_i = 1$ iff $i$ is in $ S $.
+*/
+
+
+// #include ... LogPowerSeries.cpp
 vl exp(vl A , int n){
 	assert(A[0] == 0 && sz(A));
 	vl ans = {1};
